73df851c56fe3f15b069459e07bb7a07620e15c1
SuperJames888 <james1122334454@hotmail.com>
2024-10-07 15:10:51 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-import { database, ref, get, child } from "firebase/database";
+import { database, ref, get, child } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js;"
 
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
   e.preventDefault();
