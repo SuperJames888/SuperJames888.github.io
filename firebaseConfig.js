851c55fca58f0ab847b9a08743a7637fa9f51821
SuperJames888 <james1122334454@hotmail.com>
2024-10-07 15:52:16 -0600

    Update and rename Firebase.js to firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC6B7BmKFpAEOw_ffJjo9aTQN961KzD1O0",
+  authDomain: "super-james-888-github-site.firebaseapp.com",
+  databaseURL: "https://super-james-888-github-site-default-rtdb.firebaseio.com",
+  projectId: "super-james-888-github-site",
+  storageBucket: "super-james-888-github-site.appspot.com",
+  messagingSenderId: "508751000584",
+  appId: "1:508751000584:web:13b0695fae30862ede0dbd",
+  measurementId: "G-39251VY1Y8"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const analytics = getAnalytics(app);
+
+export { database };
