f41e8c81e7cc8a98c69a72ec4cfb1e0ddb9a32fd
SuperJames888 <james1122334454@hotmail.com>
2024-10-07 16:38:37 -0600

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "./node_modules/@firebase/app";
+import { getDatabase } from "./node_modules/@firebase/database";
+import { getAnalytics } from "./node_modules/@firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyC6B7BmKFpAEOw_ffJjo9aTQN961KzD1O0",
+  authDomain: "super-james-888-github-site.firebaseapp.com",
+  databaseURL: "https://super-james-888-github-site-default-rtdb.firebaseio.com",
+  projectId: "super-james-888-github-site",
+  storageBucket: "super-james-888-github-site.appspot.com",
+  messagingSenderId: "508751000584",
+  appId: "1:508751000584:web:13b0695fae30862ede0dbd",
+  measurementId: "G-39251VY1Y8"
 };
 
 const app = initializeApp(firebaseConfig);
