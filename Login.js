e9c163ab73f9d780183fa2ec1396b537e1c206ee
SuperJames888 <james1122334454@hotmail.com>
2024-10-07 14:33:54 -0600

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,7 +1,5 @@
 import { database, ref, get, child } from "firebase/database";
-import firebaseConfig from './firebase.js';
 
-firebase.initializeApp(firebaseConfig);
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
   e.preventDefault();
   const password = document.getElementById('password').value;
